2d69be11225e376632d9410e4d94e0027477d370
Matthew Hogan <matt@matthogan.co.uk>
2014-10-20 14:42:38 +0100

    Update UUIDManager.java

--- src/sexy/canbe/crackedserverlogin/util/UUIDManager.java
+++ src/sexy/canbe/crackedserverlogin/util/UUIDManager.java
@@ -7,4 +7,17 @@ import java.util.HashSet;
 /**
  * @author Matthew
  */
-public class UUIDManager {}
+public class UUIDManager {
+  
+  private static HashSet<Player> loggedInPlayers = new HashSet<>();
+  
+  private Player player;
+  
+  public UUIDManager(Player player) {
+    this.player = player;  
+  }
+  
+  public static HashSet<Player> getLoggedInPlayers() {
+    return loggedInPlayers;
+  }
+}
